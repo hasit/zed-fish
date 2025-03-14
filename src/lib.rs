85800c299c3f325f976dca46894e13823122b5cd
ClanEver <562211524@qq.com>
2025-03-14 19:41:51 +0800

    send initializationOptions to fish-lsp

--- src/lib.rs
+++ src/lib.rs
@@ -80,6 +80,18 @@ impl zed::Extension for FishExtension {
             env: Default::default(),
         })
     }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<Option<zed::serde_json::Value>> {
+        let settings = zed::settings::LspSettings::for_worktree(server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
 }
 
 zed::register_extension!(FishExtension);
